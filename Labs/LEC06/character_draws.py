c15b3e7c1ab40817ee1e843929aaaba28b4a04f2
2023180020 <sjinho407@tukorea.ac.kr>
2026-09-22 14:43:12 +0900

    전체적인 움직임 구조 완성. 원-사각형-삼각형 이동기본 뼈대 마련.

--- Labs/LEC06/character_draws.py
+++ Labs/LEC06/character_draws.py
@@ -1,3 +1,19 @@
 # 실습 과제 진행
+def move_circle():
+    print('circle')
+    pass
+
+def move_rectangle():
+    print('rectangle')
+    pass
+
+def move_triangle():
+    print('triangle')
+    pass
+
 while True:
+    move_circle()
+    move_rectangle()
+    move_triangle()
+
     pass
